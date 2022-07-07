afa752b793c77996f2024736313202f2c8f2ccdd
b.bastieri <b.bastieri@live.com>
2022-07-07 12:00:12 -0300

    primer desafio complementario

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,7 @@
+let numeroAmultiplicar = parseInt(prompt("Hola! Qué número querés multiplicar?"));
+
+for(let i=1; i<=10; i++){
+    let resultadoMultiplicacion = numeroAmultiplicar * i;
+    console.log(resultadoMultiplicacion)
+}
 
